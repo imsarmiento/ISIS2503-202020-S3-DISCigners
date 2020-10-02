0b4e3a57d25af519984d5812c0ff0c17bbe87dc5
Isabela <im.sarmieento@uniandes.edu.co>
2020-10-02 00:07:36 -0500

    Funcion de basica generar estadisticas

---
+++ manejador_estadisticas/booklick/logic/generarTrack.py
@@ -0,0 +1,14 @@
+from usuarios.models import Actividad
+
+
+def calcular_track(estudiantep):
+    track = ""
+    actividades = Actividad.objects.filter(estudiante=estudiantep)
+    for actividad in actividades:
+        track += actividad.idActividad + ", "
+    return track
+
+
+def generar_track(estudiantep):
+    lista = calcular_track(estudiantep)
+    Actividad(nombre="Prueba", lista_actividad=lista, estudiante=estudiantep)
--- manejador_estadisticas/booklick/models.py
+++ manejador_estadisticas/booklick/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from usuarios.models import Estudiante
 from django.contrib.postgres.fields import ArrayField
+from .logic import generarTrack
 
 
 class Track(models.Model):
     nombre = models.CharField(max_length=50)
-    lista_actividad = ArrayField(ArrayField(models.IntegerField()))
+    lista_actividad = models.CharField(max_length=50)
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    #lista_prueba = generarTrack.calcularTrack(estudiante)
 
     def __str__(self):
         return '%s %s %s' % (self.nombre, self.lista_actividad, self.estudiante)
