00520f0fe52bc06dfd7a4077864fb883635773bd
Isabela <im.sarmieento@uniandes.edu.co>
2020-10-07 15:07:56 -0500

    Modelos de usuario

--- manejador_estadisticas/manejador_usuarios/models.py
+++ manejador_estadisticas/manejador_usuarios/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de Universidad
+
+
+class Universidad(models.Model):
+    nombre = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+
+
+# Modelo de Usuario
+# Clase abstracta
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    intereses = models.TextField()
+
+    class Meta:
+        abstract = True
+
+    def _str_(self):
+        return '%s %s' % (self.nombre, self.apellido)
+
+
+# Modelo de Estudiante
+# Instanciacion de usuario
+
+
+class Estudiante(Usuario):
+    carrera = models.CharField(max_length=50)
+    codigo = models.IntegerField()
