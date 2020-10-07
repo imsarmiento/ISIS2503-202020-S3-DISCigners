609da9994662b937b4789ae07861496e07b25ce7
Leonardo Vasquez <lvasquezpedreros@gmail.com>
2020-10-07 17:49:32 -0500

    Correcion routers db

--- manejador_estadisticas/manejador_estadisticas/db_routers/busquedas_router.py
+++ manejador_estadisticas/manejador_estadisticas/db_routers/busquedas_router.py
@@ -1,26 +1,35 @@
-import random
-
-class BusquedasRouter:
+class ContenidoRouter:
     def db_for_read(self, model, **hints):
         """
         Lee de la base de datos busquedas
         """
-        return 'busquedas'
+        if model._meta.app_label is 'manejador_contenido':
+            return 'busquedas_db'
+        return None
 
     def db_for_write(self, model, **hints):
         """
         Escribe la base de datos busquedas
         """
-        return 'busquedas'
+        if model._meta.app_label is 'manejador_contenido':
+            return 'busquedas_db'
+        return None
 
     def allow_relation(self, obj1, obj2, **hints):
         """
         Permite todo tipo de relaciones
         """
-        return True
+        if (
+            obj1._meta.app_label is 'manejador_busquedas' or
+            obj2._meta.app_label is 'manejador_busquedas'
+        ):
+           return True
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
         Permite migraciones de busquedas
         """
-        return db == 'busquedas'
+        if model._meta.app_label is 'manejador_busquedas':
+            return db is 'busquedas_db'
+        return None
--- manejador_estadisticas/manejador_estadisticas/db_routers/contenido_router.py
+++ manejador_estadisticas/manejador_estadisticas/db_routers/contenido_router.py
@@ -1,26 +1,35 @@
-import random
-
 class ContenidoRouter:
     def db_for_read(self, model, **hints):
         """
         Lee de la base de datos contenido
         """
-        return 'contenido'
+        if model._meta.app_label is 'manejador_contenido':
+            return 'contenido_db'
+        return None
 
     def db_for_write(self, model, **hints):
         """
         Escribe la base de datos contenido
         """
-        return 'contenido'
+        if model._meta.app_label is 'manejador_contenido':
+            return 'contenido_db'
+        return None
 
     def allow_relation(self, obj1, obj2, **hints):
         """
         Permite todo tipo de relaciones
         """
-        return True
+        if (
+            obj1._meta.app_label is 'manejador_contenido' or
+            obj2._meta.app_label is 'manejador_contenido'
+        ):
+           return True
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
         Permite migraciones de contenido
         """
-        return db == 'contenido'
+        if model._meta.app_label is 'manejador_contenido':
+            return db is 'contenido_db'
+        return None
