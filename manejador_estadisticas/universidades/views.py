0c1b01dc6176357f6fe142b8675feb341e5efd10
Vhanessa <v.cardonac1@uniandes.edu.co>
2020-10-08 23:52:22 -0500

    requerimiento

--- manejador_estadisticas/universidades/views.py
+++ manejador_estadisticas/universidades/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .logic.consultaDB import get_Consultas
 from manejador_contenido.models import Contenido
+from collections import Counter
 import csv
 
 def get_basesDatos(request):
@@ -10,14 +11,29 @@ def get_basesDatos(request):
     writer = csv.writer(response)
 
 
-    writer.writerow(['Contenido', 'Proveedor'])
-
+    writer.writerow(['Proveedor', 'NumeroConsultas'])
+    arreglo = []
+    frecuencia = []
+    diferentes = []
     for consulta in get_Consultas().values_list('estudiante', 'contenido', 'fecha'):
         proveedor =''
         for contenido in Contenido.objects.all():
             if contenido.get_Titulo() == consulta[1]:
                 proveedor = contenido.get_proveedor()
-        writer.writerow([consulta[1]+','+proveedor])
+        arreglo.append(proveedor)
+    diferentes = list(Counter(arreglo).keys())
+    frecuencia = list(Counter(arreglo).values())
+
+    i = 0
+    lista = []
+    while i < len(diferentes):
+        actual = diferentes[i] + ',' + str(frecuencia[i])
+        lista.append(tuple(map(str,actual.split(','))))
+        i += 1
+    nuevaLista = sorted(lista, key = lambda x: x[1], reverse=True)
+
+    for tupla in nuevaLista:
+        writer.writerow(tupla)
 
     response['Content-Disposition'] = 'attachment; filename="DB_Consultadas.csv"'
 
