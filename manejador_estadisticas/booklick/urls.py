a8464cfd1615f8127f36c3d4292227743163b129
Daniela Gonzalez <d.gonzalezc1@uniandes.edu.co>
2020-10-15 23:25:10 -0500

    merge si

--- manejador_estadisticas/booklick/urls.py
+++ manejador_estadisticas/booklick/urls.py
@@ -10,15 +10,8 @@ urlpatterns = [
          name="booklistsPromedioContenido_db_post"),
     path('booklistsPromedioContenido_db_get/', views.get_booklists_contenidoPromedio_db,
          name="booklistsPromedioContenido_db_get"),
-<<<<<<< HEAD
      path('booklistsCarrera_db_post/', views.post_booklists_carrera_db,
          name="booklistsCarrera_db_post"),
     path('booklistsCarrera_db_get/', views.get_booklists_carrera_db,
          name="booklistsCarrera_db_get"),
-=======
-    path('booklistsRango_db_post/', views.post_booklists_rangos_db,
-         name="booklistsRangos_db_post"),
-    path('booklistsRango_db_get/', views.get_booklists_rangos_db,
-         name="booklistsRangos_db_get"),
->>>>>>> e2eaea0110f6ad3dfc65901a1f7647789030c7d0
 ]
