f99d09b7f87f0a3996d67c4cf10c139f53bc3ae5
Leonardo Vasquez <lvasquezpedreros@gmail.com>
2020-10-15 20:16:12 -0500

    Get booklists carrera

--- manejador_estadisticas/booklick/views.py
+++ manejador_estadisticas/booklick/views.py
@@ -10,6 +10,42 @@ from .logic.booklists_carrera import get_all_booklists
 from email._header_value_parser import ContentDisposition
 from collections import Counter
 
+def get_booklists_carrera(request):
+    booklists = get_all_booklists()
+    response = HttpResponse(content_type='text/csv')
+    
+    writer = csv.writer(response)
+     
+    writer.writerow(['Carrera', 'NumBooklists'])
+     
+     
+    arreglo = []
+    frecuencua = []
+    diferentes = []
+    for booklist in booklists.values_list('titulo', 'creador', 'booklistsContenidos', 'contenidos'):
+        carrera=''
+        for estudiante in Estudiante.objects.all():
+            if estudiante.get_codigo() == booklist[1]:
+                carrera = estudiante.get_carrera()
+                
+        arreglo.append(carrera)
+    diferentes = list(Counter(arreglo).keys())
+    frecuencia = list(Counter(arreglo).values())
+    
+    i = 0
+    lista = []
+    while i < len(diferentes):
+        actual=diferentes[i]+','+str(frecuencia[i])
+        lista.append(tuple(map(str,actual.split(','))))
+        i+=1
+    nuevalista=sorted(lista,key=lambda x:int(x[1]),reverse=True)
+    
+    for tupla in nuevalista:
+        writer.writerow(tupla)
+
+    response['Content-Disposition']= 'attachment; filename="booklistsCarrera.csv"'
+        
+    return response
 
 def get_booklists(request):
 
@@ -63,6 +99,7 @@ def get_booklists(request):
     return response
 
 # Create your views here.
+
 
 
 def get_booklists_contenidoPromedio(request):
