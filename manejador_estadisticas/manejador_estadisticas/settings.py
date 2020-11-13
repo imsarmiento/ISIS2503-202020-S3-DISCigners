93d94d4d64c05056ae1c778ec876ac0ae8f1fe5f
Daniela Gonzalez <d.gonzalezc1@uniandes.edu.co>
2020-11-13 18:43:04 -0500

    intento imageen

--- manejador_estadisticas/manejador_estadisticas/settings.py
+++ manejador_estadisticas/manejador_estadisticas/settings.py
@@ -163,6 +163,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/"
