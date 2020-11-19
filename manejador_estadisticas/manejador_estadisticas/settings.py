755c90e4b5e77adca236d60c6eaf5e79a968281d
Isabela <im.sarmieento@uniandes.edu.co>
2020-11-18 22:10:39 -0500

    Pruebasde logout

--- manejador_estadisticas/manejador_estadisticas/settings.py
+++ manejador_estadisticas/manejador_estadisticas/settings.py
@@ -174,7 +174,7 @@ STATICFILES_DIRS = (
 
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "http://isis2503-leovap.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F54.227.83.83:8000"
+LOGOUT_REDIRECT_URL = "http://isis2503-leovap.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F3.218.211.113:8000"
 
 SOCIAL_AUTH_TRAILING_SLASH = False  # Remove end slash from routes
 SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503-leovap.us.auth0.com'
