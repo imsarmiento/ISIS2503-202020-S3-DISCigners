26ef132398f2cb5dae71a202dcf3a59ff409a8a7
Isabela <im.sarmieento@uniandes.edu.co>
2020-12-03 23:36:44 -0500

    Mofiicacion carpetas

--- manejador_estadisticas/manejador_usuarios/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Universidad, Estudiante
-
-admin.site.register(Universidad)
-admin.site.register(Estudiante)
-# Register your models here.
--- manejador_estadisticas/manejador_usuarios/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManejadorUsuariosConfig(AppConfig):
-    name = 'manejador_usuarios'
